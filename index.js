61c2ec288509099cfe2ea7e577ed07dfd605fd1f
Sharan358 <amudalasharansai9@gmail.com>
2026-07-12 18:52:55 +0530

    Web hook demo

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello from Node.js CI Project! - Webhook test');
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+console.log("=================================");
+console.log("Node.js CI Project");
+console.log("Continuous Integration using Jenkins");
+console.log("Build Successful!");
+console.log("=================================");
