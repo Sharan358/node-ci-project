3d5e9f6a5010fef6d3c570c9aa229a8b73c0be94
Sharan358 <amudalasharansai9@gmail.com>
2026-07-12 21:22:01 +0530

    Web hook test demo 1

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-console.log("=================================");
 console.log("Node.js CI Project");
 console.log("Continuous Integration using Jenkins");
 console.log("Build Successful!");
-console.log("=================================");
+
+// Basic math operations in Node.js
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+console.log("Addition: 5 + 3 =", add(5, 3));
+console.log("Subtraction: 10 - 4 =", subtract(10, 4));
